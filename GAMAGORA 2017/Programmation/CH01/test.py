60f8fd303299517c54a4d6eab7bdc4293e46d282
Elric <emathure@gmail.com>
2017-10-16 17:15:15 +0200

    Premier jet

---
+++ GAMAGORA 2017/Programmation/CH01/test.py
@@ -0,0 +1,7 @@
+firstName = input("What is your first name?")
+print("Nice to meet you," + firstName + ".")
+
+if firstName == "Guido":
+    print ("Hey, thanks for inventing Python!")
+else:
+    print("Have you seen Guido around?")
