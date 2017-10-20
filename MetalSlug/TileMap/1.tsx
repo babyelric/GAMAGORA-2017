2ddecf14070a578488a3a58e832cfe9a192d785f
Elric <emathure@gmail.com>
2017-10-20 08:58:41 +0200

    MàJ TileMap

---
+++ MetalSlug/TileMap/1.tsx
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="tiles_spritesheet" tilewidth="70" tileheight="70" spacing="2" tilecount="156" columns="12">
+ <image source="tiles_spritesheet.png" width="914" height="936"/>
+ <tile id="35">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="70" height="70"/>
+  </objectgroup>
+ </tile>
+ <tile id="71">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="70" height="38.6667"/>
+  </objectgroup>
+ </tile>
+</tileset>
