fda522834711f4306628fb5a5d69fba458a5ae34
Elric <emathure@gmail.com>
2018-01-06 16:50:45 +0100

    Passage SourceTree

--- Programmation/ProceduralStreet/Assets/Scripts/BuildCityNoise2.cs
+++ Programmation/ProceduralStreet/Assets/Scripts/BuildCityNoise2.cs
@@ -26,6 +26,7 @@ public class BuildCityNoise2 : MonoBehaviour {
         angles[2] = 180;
         angles[3] = 270;
 
+
         
         //float seed = Random.Range(0, 100);
 
@@ -86,13 +87,17 @@ public class BuildCityNoise2 : MonoBehaviour {
                 int result = mapgrid[w, h];
                 Vector3 pos = new Vector3(w * buildingFootprint, 0, h * buildingFootprint);
 
-				/*if(result < -2)
-                    Instantiate(crossroad,pos,crossroad.transform.rotation);
-                else if(result < -1)
-                    Instantiate(xstreets,pos,xstreets.transform.rotation);
-                else if(result < 0)
-                    Instantiate(zstreets,pos,zstreets.transform.rotation);
-                */
+
+
+				//angleChoisi = 
+
+				System.Random rnd = new System.Random();
+				angleChoisi = rnd.Next(0, 4);
+
+				Debug.Log (angleChoisi);
+
+
+
 
                 if (result < -2)
                     Instantiate(crossroad, pos, crossroad.transform.rotation);
@@ -104,58 +109,18 @@ public class BuildCityNoise2 : MonoBehaviour {
 
 
                 else if (result < 1)
-                    Instantiate(buildings[0], pos, Quaternion.Euler(new Vector3(-90, angles[angleChoisi], 0)));
+                    Instantiate(buildings[0], pos, Quaternion.Euler(new Vector3(0, angles[angleChoisi], 0)));
                 else if (result < 2)
-                    Instantiate(buildings[1], pos, Quaternion.Euler(new Vector3(-90, angles[angleChoisi], 0)));
+                    Instantiate(buildings[1], pos, Quaternion.Euler(new Vector3(0, angles[angleChoisi], 0)));
                 else if (result < 4)
-                    Instantiate(buildings[2], pos, Quaternion.Euler(new Vector3(-90, angles[angleChoisi], 0)));
+                    Instantiate(buildings[2], pos, Quaternion.Euler(new Vector3(0, angles[angleChoisi], 0)));
                 else if (result < 6)
-                    Instantiate(buildings[3], pos, Quaternion.Euler(new Vector3(-90, angles[angleChoisi], 0)));
+                    Instantiate(buildings[3], pos, Quaternion.Euler(new Vector3(0, angles[angleChoisi], 0)));
                 else if (result < 7)
-                    Instantiate(buildings[4], pos, Quaternion.Euler(new Vector3(-90, angles[angleChoisi], 0)));
+                    Instantiate(buildings[4], pos, Quaternion.Euler(new Vector3(0, angles[angleChoisi], 0)));
                 else if (result < 10)
                     Instantiate(buildings[5], pos, Quaternion.Euler(new Vector3(0, 0, 0)));
             }
-        }
-
-
-
-
-
-
-
-
-
-
-                /*angleChoisi = Random.Range(0, angles.Length);
-
-
-                int result = (int)(Mathf.PerlinNoise(w/10.0f +seed, h/10.0f +seed) * 10);
-                Vector3 pos = new Vector3(w * buildingFootprint, 0, h * buildingFootprint);
-
-                if (result < 2)
-                    Instantiate(buildings[0], pos, Quaternion.Euler(new Vector3(-90, angles[angleChoisi], 0)));
-                else if (result < 3)
-                    Instantiate(buildings[1], pos, Quaternion.Euler(new Vector3(-90, angles[angleChoisi], 0)));
-                else if (result < 5)
-                    Instantiate(buildings[2], pos, Quaternion.Euler(new Vector3(-90, angles[angleChoisi], 0)));
-                else if (result < 6)
-                    Instantiate(buildings[3], pos, Quaternion.Euler(new Vector3(-90, angles[angleChoisi], 0)));
-                else if (result < 7)
-                    Instantiate(buildings[4], pos, Quaternion.Euler(new Vector3(-90, angles[angleChoisi], 0)));
-                else if (result < 10)
-                    Instantiate(buildings[5], pos, Quaternion.Euler(new Vector3(0, 0, 0)));
-
-
-
-
-
-
-                angleChoisi = Random.Range(0, angles.Length);
-
-                Vector3 pos = new Vector3(w * buildingFootprint, 0, h * buildingFootprint);
-                int n = Random.Range(0, buildings.Length);
-                Instantiate(buildings[n], pos, Quaternion.Euler(new Vector3(-90, angles[angleChoisi], 0)));*/
-            
+        }            
 	}
 }
