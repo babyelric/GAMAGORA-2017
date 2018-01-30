48aef40608a18263690594bdef08d4e3e4f361e7
Elric <emathure@gmail.com>
2018-01-30 10:41:59 +0100

    KodaUnity

---
+++ KODA/ProjetsUnity/TanukiProject-master/Assets/Scripts/Player/InflateController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InflateController : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject inflateForm;
+    [SerializeField]
+    private GameObject normalForm;
+    private bool isInflate;
+    [SerializeField]
+    private GameObject smokeSpawner;
+
+
+    void Start() {
+        isInflate = false;
+
+    }
+
+    void Update() {
+
+
+        if (Input.GetButtonDown("Transformation")) {
+            if (isInflate) {
+                GameObject smoke = Instantiate(smokeSpawner, inflateForm.transform.position, Quaternion.identity);
+                Destroy(smoke, 4);
+                normalForm.transform.position = inflateForm.transform.position;
+                normalForm.SetActive(true);
+                inflateForm.GetComponent<Rigidbody>().velocity = Vector3.zero;
+                inflateForm.GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
+                inflateForm.transform.rotation = Quaternion.identity;
+                inflateForm.SetActive(false);
+                isInflate = false;
+            }
+            else {
+                if (normalForm.GetComponent<PlayerDistantAttack>().GetLeafIsBack()) {
+                    GameObject smoke = Instantiate(smokeSpawner, normalForm.transform.position, Quaternion.identity);
+                    Destroy(smoke, 4);
+                    inflateForm.transform.position = normalForm.transform.position;
+                    inflateForm.transform.rotation = normalForm.transform.rotation;
+                    inflateForm.GetComponent<Rigidbody>().velocity = Vector3.zero;
+                    inflateForm.SetActive(true);
+                    normalForm.SetActive(false);
+                    isInflate = true;
+                }
+
+            }
+
+
+        }
+
+    }
+}
